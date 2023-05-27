820930e6cc1e23f4a8565c1d2d075944e2ac2a60
renon <sugitarios.renon_vincent@hnu.edu.ph>
2023-05-27 14:34:16 +0800

    added form

---
+++ form.php
@@ -0,0 +1,14 @@
+<div class="steps">
+  <a href="#" class="step-item" data-bs-toggle="tooltip" title="Step 1 description">
+    Step 1
+  </a>
+  <a href="#" class="step-item" data-bs-toggle="tooltip" title="Step 2 description">
+    Step 2
+  </a>
+  <a href="#" class="step-item active" data-bs-toggle="tooltip" title="Step 3 description">
+    Step 3
+  </a>
+  <span href="#" class="step-item" data-bs-toggle="tooltip" title="Step 4 description">
+    Step 4
+  </span>
+</div>
