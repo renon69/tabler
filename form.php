62fcca5d11299b96ba272c08728afe036fd623d0
renon <sugitarios.renon_vincent@hnu.edu.ph>
2023-05-28 20:34:49 +0800

    added

--- form.php
+++ form.php
@@ -1,14 +1,29 @@
-<div class="steps">
-  <a href="#" class="step-item" data-bs-toggle="tooltip" title="Step 1 description">
-    Step 1
-  </a>
-  <a href="#" class="step-item" data-bs-toggle="tooltip" title="Step 2 description">
-    Step 2
-  </a>
-  <a href="#" class="step-item active" data-bs-toggle="tooltip" title="Step 3 description">
-    Step 3
-  </a>
-  <span href="#" class="step-item" data-bs-toggle="tooltip" title="Step 4 description">
-    Step 4
-  </span>
-</div>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Image Display Example</title>
+</head>
+<body>
+  <div>
+    <input type="file" id="imageInput" accept="image/*" />
+    <img id="imagePreview" src="#" alt="Image Preview" style="max-width: 300px; max-height: 300px; display: none;" />
+  </div>
+  
+  <script>
+    var imageInput = document.getElementById("imageInput");
+    var imagePreview = document.getElementById("imagePreview");
+
+    imageInput.addEventListener("change", function () {
+      var file = imageInput.files[0];
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        imagePreview.src = e.target.result;
+        imagePreview.style.display = "block";
+      };
+
+      reader.readAsDataURL(file);
+    });
+  </script>
+</body>
+</html>
